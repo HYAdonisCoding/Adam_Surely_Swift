b1e3f72290257df9dbd93b33c1a67014e4102e3b
Adam <296786475@qq.com>
2021-03-24 17:25:46 +0800

    group.notify

--- Adam_Surely_Swift/DispatchGroupViewController.swift
+++ Adam_Surely_Swift/DispatchGroupViewController.swift
@@ -12,9 +12,44 @@ class DispatchGroupViewController: BaseViewController {
     override func configEvent() {
         super.configEvent()
         
+        testDispatchGroup()
     }
     
     override func configUI() {
         super.configUI()
+    }
+    
+    func testDispatchGroup() {
+        let group = DispatchGroup()
+        let queue = DispatchQueue(label: "Test_Queue", qos: .background)
+        
+//        queue.async {
+//            print("work 1")
+//            group.enter()
+//            sleep(4)
+//            print("work 1 finished")
+//            group.leave()
+//        }
+//
+//        work("1", queue: queue, group: group)
+        for i in 0...100 {
+//            print(i)
+            queue.async { [self] in
+                work(String(i), queue: queue, group: group)
+            }
+        }
+        group.notify(queue: queue) {
+            print("coming")
+        }
+    }
+    
+    func work(_ name: String, queue: DispatchQueue, group: DispatchGroup) {
+//        queue.async {
+            print("work \(name)")
+            group.enter()
+            sleep(1)
+            print("work \(name) finished")
+            group.leave()
+//        }
     }
 }
